b1e01e4185b62306ddc08e625be0f726afe61962
arducraft778 <redmiax27@gmail.com>
2025-07-19 01:04:41 +0300

    'v1'

---
+++ create_a4ievements.py
@@ -0,0 +1,34 @@
+#################Єбаут#################################################
+'''
+Даний скрипт допомагає з генерацією файла ачівок для Steam з україською локалізацією
+Наклацаний (не)від всієї душі Death_Hunter'ом
+'''
+import re
+#################Шляхи до файлів#######################################
+#Оригінальний файл ачівок який знаходиться в C:\Program Files (x86)\Steam\appcache\stats\
+original_a4ieve_file = r"UserGameStatsSchema_460790.bin"
+
+#Шлях до файлу (або той же файл як в моєму випадку) в котрому зберегти модифіковані дані,
+#потім цей файл перейменувати згідно оригінального та замінити в папці вказаній вище
+extract_file = original_a4ieve_file
+
+#Текстовий файл з перекладеними ачівками
+trans_file = r"ачівки.txt"
+#######################################################################
+raw_data = open(original_a4ieve_file, 'r' , encoding='utf-8').read()
+trans_base = open(trans_file, 'r' , encoding='utf-8').read()
+trans_list = [j for v in [y.split('\n') for y in trans_base.split('\n\n')] for j in v]
+
+pat = 'german\x00.*?(?=\x00)'                                   #Не звертайте уваги на це, це НЕ заміна німецької, це для того (див. ряд. 23)
+
+def tr (pat=pat, raw_data=raw_data, trans_list=trans_list):
+    mod_data = raw_data
+    offsets = [g for g in re.finditer(pat, raw_data)]
+    for o, u in enumerate(offsets):
+        mtch = u.group()
+        insrt = f"{mtch}\x00\x01ukrainian\x00{trans_list[o]}"   #щоб після німецької українську вставити, бо англійська (в деяких іграх) перед токеном і хз шо буде якшо після англ вставити
+        mod_data = mod_data.replace(mtch, insrt)
+    return mod_data
+#################Зберення змін не забудьте про бекап####################
+with open(extract_file, 'w', encoding='utf-8') as ff:
+	ff.write(tr())
---
+++ extract_a4ievements.py
@@ -0,0 +1,27 @@
+#################Єбаут#################################################
+'''
+Даний скрипт допомагає з видобутком ачівок для Steam для подальшої українізації
+Наклацаний (не)від всієї душі Death_Hunter'ом
+'''
+import re
+#################Шляхи до файлів#######################################
+#Оригінальний файл ачівок який знаходиться в C:\Program Files (x86)\Steam\appcache\stats\
+#копійнути в робочу папку
+orig_a4ieve_file = r"UserGameStatsSchema_460790.bin"
+
+#Текстовий файл з ачівками які треба перекласти, щоб потім заюзати інший скрипт
+trans_file = r"ачівки.txt"
+#######################################################################
+raw_data = open(orig_a4ieve_file, 'r' , encoding='utf-8').read()
+
+pat = '(?<=english\x00).*?(?=\x00\x01)'
+
+def rt(pat=pat, raw_data=raw_data):
+    string = ''
+    for o, i in enumerate(re.findall(pat, raw_data)):
+        string += f'{i}\n'
+        if o % 2 == 1: string += '\n'
+    return string
+
+with open(trans_file, 'w', encoding='utf-8') as ff:
+	ff.write(rt())
